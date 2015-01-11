af8ba7cc3205a3cf61b9a4ae4e80d39f977b4a75
billytrend <billy.trend@gmail.com>
2015-01-11 04:07:34 +0000

    Done a nice layout.

---
+++ mains.js
@@ -0,0 +1,26 @@
+var modules = Object.keys(data);
+
+var findModule = function(partial) {
+    var regex = new RegExp(partial)
+    for (var i = 0; i < modules.length; i++) {
+        if (modules[i].match(regex)) {
+            return modules[i];
+        }
+    }
+    return '';
+};
+
+document.getElementById('code').oninput = function () {
+    var guess = document.getElementById('code').value;
+    if(guess in data) {
+        fill(data[guess]);
+    }
+};
+
+var fill = function(obj) {
+    Object.keys(obj).forEach(function(key) {
+        if (obj[key] instanceof Object) fill(obj[key]);
+        if (null === document.getElementById(key)) return;
+        document.getElementById(key).innerHTML = obj[key];
+    });
+};
