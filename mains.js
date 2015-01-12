831071bcf11a1338c042d1997a3d4fd685621b3b
billytrend <billy.trend@gmail.com>
2015-01-12 15:42:26 +0000

    Improved perf.

--- mains.js
+++ mains.js
@@ -14,62 +14,73 @@ var findModule = function(partial) {
 var timeout;
 
 document.getElementById('search').oninput = function () {
+
     var guess = document.getElementById('search').value;
-    var results = search(data, guess);
+
+    clearTimeout(timeout);
+
     if (guess.length > 2) {
+
         var f = function() {
+
+            var results = search(data, guess);
             addNodes(results.length);
             results.forEach(fill);
         };
-        clearTimeout(timeout);
 
-        timeout = setTimeout(f, 200);
+        timeout = setTimeout(f, 300);
     }
 
 };
 
 var fill = function(obj, i) {
+
     Object.keys(obj).forEach(function(key) {
+
         if (obj[key] instanceof Object) fill(obj[key], i);
+
         if (0 === document.getElementsByName(key).length) return;
+
         document.getElementsByName(key)[i].innerHTML = obj[key];
     });
 };
 
 var addNodes = function(i) {
+
     document.getElementById('items').innerHTML = '';
+
     if (i == 0) {
+
         document.getElementById('items').appendChild(newNode.cloneNode(true));
+
     }
     while (i-- > 0) {
+
         document.getElementById('items').appendChild(newNode.cloneNode(true));
+
     }
 };
 
 var search = function(data, needle) {
+
     results = [];
     var reg = new RegExp(needle, 'i');
+
     Object.keys(data).forEach(function(e) {
-        if (results.length === 10) return;
+
+        if (results.length == 10) return;
+
         data[e].code = e;
-        if (reg.test(e)) {
+
+        var result = reg.test(data[e].description) ||
+                reg.test(data[e].code) ||
+                reg.test(data[e].coord);
+
+        if (result) {
             results.push(data[e]);
-            return;
         }
-        Object.keys(data[e]).forEach(function(f) {
-            if (data[e][f] instanceof Object) {
-                Object.keys(data[e][f]).forEach(function(g) {
-                    if (reg.test(data[e][f][g])) {
-                        results.push(data[e]);
-                        return;
-                    }
-                });
-            }
-            else if (reg.test(data[e][f])) {
-                results.push(data[e]);
-                return;
-            }
-        });
+
     });
+
     return results;
 };
